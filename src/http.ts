9184f8ca0a1d062a4d41dcfae28c2a1d425b24fa
Victor Turra <victor.florencio@univem.edu.br>
2021-11-28 15:43:13 -0300

    Fix cors configuration

--- src/http.ts
+++ src/http.ts
@@ -1,19 +1,31 @@
 import express from "express";
-import http from 'http'
-import {Server} from 'socket.io'
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Headers", "*");
+  res.set("Access-Control-Allow-Methods", "*");
+  next();
+});
+
 app.get("/", (req, res) =>
   res.status(200).json({ hello: "Server is up time" })
 );
 
-app.post('/auth', async (req, res) => {
-  return res.status(200).json({})
-})
+app.post("/auth", async (req, res) => {
+  return res.status(200).json({});
+});
 
-const httpServer = http.createServer(app)
-const io = new Server(httpServer)
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+    methods: ['*']
+  }
+});
 
-export { httpServer, io }
+export { httpServer, io };
