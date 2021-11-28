776bfccb058d9f816a8433e38902a1b917927907
Victor Turra <victor.florencio@univem.edu.br>
2021-11-27 21:07:06 -0300

    Create express server and ts config

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log("Hello world")
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.get("/", (req, res) =>
+  res.status(200).json({ hello: "Server is up time" })
+);
+
+app.listen(3333, () => console.log("Server running on port 3333"));
