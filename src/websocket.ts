4a683a9469aa9d0e17f676c1b8505ff68ba1962d
Victor Turra <victor.florencio@univem.edu.br>
2021-11-28 15:43:27 -0300

    Implement room section

--- src/websocket.ts
+++ src/websocket.ts
@@ -1,5 +1,30 @@
 import { io } from "./http";
+import { RoomService } from "./services/room_service";
 
-io.on("connection", socket => {
-  console.log("socket connected: ", socket.id )
-})
+const roomService = new RoomService();
+
+io.on("connection", (socket) => {
+  console.log("socket connected: ", socket.id);
+
+  socket.on("select_room", (data) => {
+    const room_name = `${data.user.nickname}_${data.contact.nickname}`;
+    socket.join(room_name);
+
+    const userInRoom = roomService.findWhere({
+      user_id: data.user.id,
+      contact_id: data.contact.id,
+    });
+
+    if (userInRoom) {
+      userInRoom.socket_id = socket.id;
+    } else {
+      roomService.create({
+        user_id: data.user.id,
+        contact_id: data.contact.id,
+        type: data.type,
+        room_name,
+        socket_id: socket.id,
+      });
+    }
+  });
+});
