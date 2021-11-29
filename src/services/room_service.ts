528c9809d619a310105c5aad123fc3969a2776c2
Victor Turra <victor.florencio@univem.edu.br>
2021-11-28 23:38:24 -0300

    Add find rooms by user id method

--- src/services/room_service.ts
+++ src/services/room_service.ts
@@ -1,19 +1,36 @@
 export interface Room {
   id?: number;
   type: "contact" | "group";
-  user_id: string;
-  contact_id: string;
+  user_id: number;
+  contact_id: number;
   room_name: string;
   socket_id: string;
 }
 
 export interface Where {
-  user_id: string;
-  contact_id: string;
+  user_id: number;
+  contact_id: number;
 }
 
 export class RoomService {
-  private rooms: Room[] = [];
+  private rooms: Room[] = [
+    {
+      id: 1,
+      user_id: 1,
+      contact_id: 2,
+      type: "contact",
+      room_name: "VictorTurraF_AmandaTF",
+      socket_id: "pCa4ETzD4WUzzEffAAAD",
+    },
+    {
+      id: 2,
+      user_id: 1,
+      contact_id: 3,
+      type: "contact",
+      room_name: "VictorTurraF_GagrielTF",
+      socket_id: "pCa4ETzD4WUzzEffAAAD",
+    },
+  ];
 
   public create(data: Room) {
     const newRoom = { id: this.getNextId(), ...data };
@@ -28,6 +45,12 @@ export class RoomService {
         room.user_id === condition.user_id
       );
     });
+  }
+
+  public findByUserId(user_id: number) {
+    return this.rooms.filter(
+      (room) => room.user_id === user_id || room.contact_id === user_id
+    );
   }
 
   private getNextId() {
